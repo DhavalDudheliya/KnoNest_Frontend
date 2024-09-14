f5a351175491ab2010edd2c6a577c3a068535c41
Dhaval Dudheliya <dhavaldudheliya77@gmail.com>
2024-09-14 10:16:37 +0530

    commit

--- myConstant.ts
+++ myConstant.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "https://knonest-backend.onrender.com/api";
+export const BACKEND_URL = "https://knonest-backend.onrender.com";
 
 // export const BACKEND_URL = "http://localhost:5000/api";
